366e7336c0115139c2a1e4de2571db4992914499
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-12-10 22:00:52 -0300

    Create Step component

---
+++ src/components/step/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { s } from "./styles";
+
+type Props = {
+  title: string
+  description: string
+}
+
+export function Step({ title, description }: Props) {
+  return (
+    <View style={s.container}>
+      <View style={s.details}>
+        <Text style={s.title}>{title}</Text>
+        <Text style={s.description}>{description}</Text>
+      </View>
+    </View>
+  )
+}
