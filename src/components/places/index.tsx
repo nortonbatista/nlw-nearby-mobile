c03562d8e5708bf00d3eec21c8c38162500c9375
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-12-11 22:48:49 -0300

    Create Places component

---
+++ src/components/places/index.tsx
@@ -0,0 +1,40 @@
+import { Text, View, useWindowDimensions } from "react-native"
+import { Place, PlaceProps } from "../place"
+import { useRef } from "react"
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
+import { s } from "./styles"
+
+type Props = {
+  data: PlaceProps[]
+}
+
+export function Places({ data }: Props) {
+  const dimensions = useWindowDimensions()
+  const bottomSheetRef = useRef<BottomSheet>(null)
+  
+  const snapPoints = {
+    min: 278,
+    max: dimensions.height - 128
+  }
+
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={[snapPoints.min, snapPoints.max]}
+      handleIndicatorStyle={s.indicator}
+      backgroundStyle={s.container}
+      enableOverDrag={false}
+    >
+      <BottomSheetFlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Place data={item} />}
+        contentContainerStyle={s.content}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={() => (
+          <Text style={s.title}>Explore locais perto de você</Text>
+        )}
+      />
+    </BottomSheet>
+  )
+}
