79136d70d1a19cfe852e01307b0bb1ac1ff11f74
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-12-11 22:34:07 -0300

    Add React Native Bottom Sheet

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,6 +9,7 @@ import {
   Rubik_700Bold
 } from "@expo-google-fonts/rubik"
 import { Loading } from "@/components/loading"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -21,11 +22,13 @@ export default function Layout() {
   if(!fontsLoaded) return <Loading />
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.gray[100] }
-      }}
-    />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] }
+        }}
+      />
+    </GestureHandlerRootView>
   )
 }
