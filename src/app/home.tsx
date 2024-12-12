184260a919797b1a6f35ffcc550412c63b743501
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2024-12-11 22:49:23 -0300

    Fetch places by category and reder them at Home

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,11 +1,16 @@
 import { Categories, CategoryProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native";
 
+type MarketsProps = PlaceProps & {}
+
 export default function Home() {
-  const [categories, setCategories] = useState<CategoryProps[]>([])
   const [category, setCategory] = useState('')
+  const [categories, setCategories] = useState<CategoryProps[]>([])
+  const [markets, setMarkets] = useState<MarketsProps[]>([])
 
   async function fetchCategories() {
     try {
@@ -18,13 +23,35 @@ export default function Home() {
     }
   }
 
+  async function fetchMarkets() {
+    try {
+      if (!category) return
+
+      const { data } = await api.get(`/markets/category/${category}`)
+      setMarkets(data)
+    } catch (error) {
+      console.error(error)
+      Alert.alert("Locais", "Não foi possível carregar os locais")
+    }
+  }
+
   useEffect(() => {
     fetchCategories()
   }, [])
 
+  useEffect(() => {
+    fetchMarkets()
+  }, [category])
+
   return (
-    <View style={{ flex: 1}}>
-      <Categories data={categories} selected={category} onSelect={setCategory} />
+    <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+      <Categories
+        data={categories}
+        selected={category}
+        onSelect={setCategory}
+      />
+
+      <Places data={markets} />
     </View>
   )
 }
